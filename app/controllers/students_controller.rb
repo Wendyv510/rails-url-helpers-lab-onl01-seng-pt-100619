dcd06a3e4f20ea4e7e4c6053b53c7e641f9bc236
Wendy Harris <wendyv510@hotmail.com>
2020-03-05 01:47:18 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,9 +4,23 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+  
+  def new 
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name], active: params[:active])
+    @student.save 
+  end 
 
   def show
+    @student = Student.find(params[:id])
   end
+  
+  def activate 
+    @student = Student.find(params[:id])
+    @student.active = !@student.active  
+    @student.save 
+    
+       redirect_to student_path(@student)  
+  end 
 
   private
 
